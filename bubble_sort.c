82af6f59fd63029330deaa135663aa1f30f7b498
sunny01sg <sunny.sg2311@gmail.com>
2025-11-18 19:55:51 +0530

    Add bubble sort implementation in bubble_sort.c
    Implement bubble sort algorithm and array printing.

---
+++ bubble_sort.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    int swapped;
+
+    for (i = 0; i < n - 1; i++) {
+        swapped = 0;
+
+        for (j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = 1;
+            }
+        }
+
+        if (swapped == 0) {
+            break;
+        }
+    }
+}
+
+void printArray(int arr[], int size) {
+    int i;
+    for (i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int data[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(data) / sizeof(data[0]);
+
+    printf("Array before sorting: \n");
+    printArray(data, n);
+
+    bubbleSort(data, n);
+
+    printf("Array after sorting (Ascending): \n");
+    printArray(data, n);
+
+    return 0;
+}
