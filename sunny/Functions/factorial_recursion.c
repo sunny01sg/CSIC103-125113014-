9fa11f3591c3bf9d6b580071b2995e03d6d34b15
sunny01sg <sunny.sg2311@gmail.com>
2025-11-25 00:40:53 +0530

    factorial_recursion.c

---
+++ sunny/Functions/factorial_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+long factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num < 0)
+        printf("Factorial is not defined for negative numbers.\n");
+    else
+        printf("Factorial of %d = %ld\n", num, factorial(num));
+
+    return 0;
+}
