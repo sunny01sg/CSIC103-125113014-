a67ad4615f13f870eb063b50ea1345b75decf724
sunny01sg <sunny.sg2311@gmail.com>
2025-11-25 00:41:39 +0530

    gcd_function.c

---
+++ sunny/Functions/gcd_function.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int num1, num2;
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    printf("GCD of %d and %d = %d\n", num1, num2, gcd(num1, num2));
+
+    return 0;
+}
